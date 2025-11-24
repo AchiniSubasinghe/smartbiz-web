bfba4f4b8aa9777da5b35b91779abe27a65c1084
AchiniSubasinghe <achinimaheshika12@gmail.com>
2025-11-24 17:52:23 +0530

    feat: redirect based on the role

--- src/Components/auth/ProtectedRoute.jsx
+++ src/Components/auth/ProtectedRoute.jsx
@@ -1,9 +1,15 @@
 import { Navigate } from "react-router-dom";
 
-export default function ProtectedRoute({ children }) {
-  const user = localStorage.getItem("user");
+export default function ProtectedRoute({ children, allowedRoles }) {
+  const user = JSON.parse(localStorage.getItem("user"));
 
+  // Not logged in → redirect to login
   if (!user) return <Navigate to="/login" replace />;
+
+  // Role not allowed → redirect to profile
+  if (allowedRoles && !allowedRoles.includes(user.role)) {
+    return <Navigate to="/profile" replace />;
+  }
 
   return children;
 }
